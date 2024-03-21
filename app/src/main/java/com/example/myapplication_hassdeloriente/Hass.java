07ad6e9e9e3b358269428bd2b8cb9934e3feebe1
Duber <dubercarmona87@gmail.com>
2024-03-21 10:09:17 -0500

    first commit

---
+++ app/src/main/java/com/example/myapplication_hassdeloriente/Hass.java
@@ -0,0 +1,15 @@
+package com.example.myapplication_hassdeloriente;
+
+public class Hass {
+    public String producto;
+    public String cantidad;
+    public String referencia;
+
+// Constructor para crear produto
+    public  Hass(String producto, String cantidad, String referencia ){
+
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.referencia = referencia;
+}
+}
